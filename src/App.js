6cefa56fe40482d701688bfe2fab37f0117f08e8
Mark J. Mononutu <s21810053@student.unklab.ac.id>
2021-04-12 21:28:48 +0800

    Exercise 6

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import {View} from 'react-native';
+import ScreenRegister from './component/Pages/Register';
+import ScreenUserList from './component/Pages/UserList';
+
+const App = () => {
+  return (
+    <>
+      {/* <ScreenRegister /> */}
+      <ScreenUserList />
+    </>
+  );
+};
+
+export default App;
